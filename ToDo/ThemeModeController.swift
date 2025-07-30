8637bd96e26755b3a15c294f8bb6038acfd994b8
Max Gabriel <Max.Gabriel@triosstudent.com>
2025-07-30 19:27:31 -0400

    fixed bug where the controls for theme werent showing current interface theme properly

--- ToDo/ThemeModeController.swift
+++ ToDo/ThemeModeController.swift
@@ -12,12 +12,28 @@ class ThemeModeController: UIViewController {
   @IBOutlet weak var appTheme: UILabel!
   @IBOutlet weak var settingsModal: UIView!
   @IBOutlet var settingsView: UIView!
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
   
     override func viewDidLoad() {
         super.viewDidLoad()
       themeHeadLabel.font = UIFont.style(.category)
       appTheme.font = UIFont.style(.caption)
       settingsModal.backgroundColor = .secondarySystemBackground
+      
+      //detecting what mode device is on atm
+      let window = UIApplication.shared.connectedScenes.flatMap {($0 as? UIWindowScene)?.windows ?? [] }.first {$0.isKeyWindow}
+      if let window = window {
+        switch window.overrideUserInterfaceStyle {
+          case .light:
+            segmentedControl.selectedSegmentIndex = 0
+          case .dark:
+            segmentedControl.selectedSegmentIndex = 1
+          case .unspecified:
+            segmentedControl.selectedSegmentIndex = 2
+          @unknown default: //for cases which are there but we dont know about those like cusmot user themes on the phone loaded from net
+            segmentedControl.selectedSegmentIndex = 2
+        }
+      }
 //      settingsView.backgroundColor = .systemBackground.withAlphaComponent(0.95) //change it in storyBoard or programmatically here
     }
     
